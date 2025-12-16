f16f4f9b48e686c37e7bce6c63a6191a6640ddda
181172-dot <181172@noda.ed.jp>
2025-12-16 10:47:50 +0900

    Initialize WebSocket server with Express

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let answers = {};
+let opened = false;
+
+app.use(express.static("public"));
+
+wss.on("connection", ws => {
+  ws.send(JSON.stringify({ type:"init", answers, opened }));
+
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
+
+    if(data.type === "answer"){
+      answers[data.name] = data.answer;
+    }
+    if(data.type === "open"){
+      opened = true;
+    }
+    if(data.type === "reset"){
+      answers = {};
+      opened = false;
+    }
+
+    wss.clients.forEach(c=>{
+      c.send(JSON.stringify({
+        type:"update",
+        answers,
+        opened
+      }));
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3000);
